2f53a78ac9f1e381451850cc847dec309e2a046d
Pawel Lisewski <pawel.lisewski@gmail.com>
2019-08-05 18:45:15 +0800

    create PlayerSet

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,18 @@ import React from 'react';
 import styled from 'styled-components/macro';
 import {Normalize} from 'styled-normalize';
 
-const side = '27vh';
+const side = 21;
+const unit = 'vh';
+
+const Player = styled.div`
+  background: orange;
+  height: ${side + unit};
+  width: ${side * 2 + unit};
+  border: 1px solid grey;
+  transform-origin: 50% 50%;
+  display: flex;
+  flex-direction: column;
+`;
 
 const Wrapper = styled.div`
   width: 100vw;
@@ -20,12 +31,15 @@ const Left = styled.div`
   position: absolute;
   top: 0;
   left: 0;
-  width: ${side};
+  width: ${side + unit};
   height: 100vh;
   display: flex;
   flex-direction: column;
   justify-content: center;
   background: lightgrey;
+  & ${Player} {
+    transform: translateX(-50%) rotate(90deg) translateY(-50%);
+  }
 `;
 
 const Right = styled.div`
@@ -33,12 +47,15 @@ const Right = styled.div`
   position: absolute;
   top: 0;
   right: 0;
-  width: ${side};
+  width: ${side + unit};
   height: 100vh;
   display: flex;
   flex-direction: column;
   justify-content: center;
   background: lightgrey;
+  & ${Player} {
+    transform: translateX(-50%) rotate(-90deg) translateY(50%);
+  }
 `;
 
 const Top = styled.div`
@@ -47,11 +64,15 @@ const Top = styled.div`
   top: 0;
   left: 0;
   width: 100vw;
-  height: ${side};
+  height: ${side + unit};
   display: flex;
   flex-direction: row;
   justify-content: center;
   background: lightgrey;
+  & ${Player} {
+    transform: rotate(180deg);
+    margin: 0 auto;
+  }
 `;
 
 const Bottom = styled.div`
@@ -60,60 +81,63 @@ const Bottom = styled.div`
   bottom: 0;
   left: 0;
   width: 100vw;
-  height: ${side};
+  height: ${side + unit};
   display: flex;
   flex-direction: row;
   justify-content: space-between;
   background: lightgrey;
+  & ${Player} {
+    margin: 0 auto;
+  }
 `;
 
-const Player = styled.div`
-  background: orange;
-  height: ${side};
-  width: 40vh;
-  border: 1px solid grey;
-  transform-origin: 50% 50%;
+const TileRow = styled.div`
   display: flex;
-  flex-direction: column;
-  justify-content: flex-end;
+  flex-direction: row;
+  width: 100%;
+  height: 50%;
 `;
 
-const PlayerLeft = styled(Player)`
-  transform: translateX(-50%) rotate(90deg) translateY(-50%);
+const Tile = styled.div`
+  width: 25%;
+  height: 100%;
+  border: 1px solid green;
 `;
 
-const PlayerRight = styled(Player)`
-  transform: translateX(-50%) rotate(-90deg) translateY(50%);
-`;
-
-const PlayerTop = styled(Player)`
-  transform: rotate(180deg);
-  margin: 0 auto;
-`;
-
-const PlayerBottom = styled(Player)`
-  margin: 0 auto;
-`;
+const PlayerSet: React.FC = () => (
+  <Player>
+    <TileRow>
+      <Tile>Text</Tile>
+      <Tile>Text</Tile>
+      <Tile>Text</Tile>
+      <Tile>Text</Tile>
+    </TileRow>
+    <TileRow>
+      <Tile>Text</Tile>
+      <Tile>Text</Tile>
+      <Tile>Text</Tile>
+      <Tile>Text</Tile>
+    </TileRow>
+  </Player>
+);
 
 const App: React.FC = () => {
   return (
     <Wrapper>
       <Normalize />
       <Left>
-        <PlayerLeft>Left</PlayerLeft>
+        <PlayerSet />
       </Left>
       <Right>
-        <PlayerRight>Right</PlayerRight>{' '}
+        <PlayerSet />
       </Right>
       <Top>
-        <PlayerTop>First</PlayerTop>
-        <PlayerTop>First</PlayerTop>
-        <PlayerTop>First</PlayerTop>
+        <PlayerSet />
+        <PlayerSet />
       </Top>
       <Bottom>
-        <PlayerBottom>First</PlayerBottom>
-        <PlayerBottom>First</PlayerBottom>
-        <PlayerBottom>First</PlayerBottom>
+        <PlayerSet />
+        <PlayerSet />
       </Bottom>
     </Wrapper>
   );
